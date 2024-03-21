3109e53a57f66039f7839b132fb81b001f487bf6
webflyx <zavala180395@gmail.com>
2024-03-21 13:21:15 +0200

    Add vue3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue'
+import App from "./components/App.vue";
+
+const app = createApp({})
+
+app.component('app', App)
+
+app.mount('#app')
---
+++ resources/js/components/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+Hello
+</template>
+
+<style scoped>
+
+</style>
