7b8097e088002e97fe2a007e8d4d048aa782e5d0
webflyx <zavala180395@gmail.com>
2024-03-21 12:58:30 +0200

    Add localization

---
+++ lang/en/response.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Something went wrong' => 'Something went wrong',
+    'You cannot sign with those credentials' => 'You cannot sign with those credentials',
+    'Registration completed successfully' => 'Registration completed successfully',
+    'New project created' => 'New project created',
+    'Project updated' => 'Project updated',
+    'Project deleted' => 'Project deleted',
+    'New task created' => 'New task created',
+    'Task updated' => 'Task updated',
+    'Task deleted' => 'Task deleted'
+];
